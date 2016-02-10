97e4b355dd64204af383e90f08311edfa39b6ec9
Angelo Nuffer <angelonuffer@gmail.com>
2016-02-10 09:40:00 -0200

    Create dynwindow.

--- scripts/base.js
+++ scripts/base.js
@@ -131,6 +131,72 @@ function Menu(actions) {
 	element.querySelector("a:first-child").style.borderBottomLeftRadius = "5px";
 };
 
+function DynWindow() {
+	var element = document.createElement("div");
+	BaseObject.call(this, element);
+	var title = document.createElement("div");
+	var content = document.createElement("div");
+	var close = document.createElement("a");
+	close.innerHTML = "X";
+	close.style.cursor = "default";
+	close.style.padding = "7px";
+	close.style.borderStyle = "solid";
+	close.style.borderColor = "black";
+	close.style.borderWidth = "1px";
+	close.style.borderTopLeftRadius = "4px";
+	close.addEventListener("click", this.remove);
+	title.style.cursor = "default";
+	title.style.paddingTop = "9px";
+	title.style.paddingBottom = "9px";
+	title.style.paddingLeft = "1px";
+	title.style.borderBottomStyle = "solid";
+	title.style.borderColor = "black";
+	title.style.borderWidth = "1px";
+	content.style.borderStyle = "solid";
+	content.style.borderColor = "black";
+	content.style.borderWidth = "1px";
+	content.style.borderBottomLeftRadius = "4px";
+	content.style.borderBottomRightRadius = "4px";
+	content.style.margin = "1px";
+	element.style.fontFamily = "Arial";
+	element.style.position = "absolute";
+	element.style.top = "30%";
+	element.style.left = "50%";
+	element.style.width = "400px";
+	element.style.marginLeft = "-200px";
+	element.style.backgroundColor = "white";
+	element.style.borderStyle = "solid";
+	element.style.borderColor = "black";
+	element.style.borderWidth = "1px";
+	element.style.borderRadius = "5px";
+	function windowMove(event) {
+		element.style.left = event.clientX + "px";
+		element.style.top = window.scrollY + event.clientY - 20 + "px";
+	};
+	function windowDrag(event) {
+		title.style.cursor = "move";
+		window.addEventListener("mousemove", windowMove);
+	};
+	function windowDrop(event) {
+		title.style.cursor = "default";
+		window.removeEventListener("mousemove", windowMove);
+	};
+	title.addEventListener("mousedown", windowDrag);
+	title.addEventListener("mouseup", windowDrop);
+	title.appendChild(close);
+	element.appendChild(title);
+	element.appendChild(content);
+	this.appendChild = function(child) {
+		content.appendChild(child);
+		if (child.field) {
+			this[child.field.id] = child.field;
+		};
+	};
+	this.removeChild = function(child) {
+		content.removeChild(child);
+	};
+};
+
 function createPrintCSS() {
 	var stylesheet = document.createElement("style");
 	stylesheet.media = "print";

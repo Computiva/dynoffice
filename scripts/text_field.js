fd7d52266a0c0b73fe426f6585848417cdcb62bb
Angelo Nuffer <angelonuffer@gmail.com>
2016-02-14 16:24:15 -0200

    Simplify text field.

--- scripts/text_field.js
+++ scripts/text_field.js
@@ -1,22 +1,11 @@
 function TextField(name, kwargs) {
-	if (!kwargs) {
-		var kwargs = new Object();
-	};
-	if (!kwargs.initial_value) {
-		kwargs.initial_value = new String();
-	};
 	var element = document.createElement("input");
-	Field.call(this, name, element, {parent: kwargs.parent});
+	Field.call(this, name, element, kwargs);
 	element.type = "text";
-	element.value = kwargs.initial_value;
 	element.style.width = "100%";
-	Object.defineProperty(this, "value", {
-		set: function(value) {
-			element.value = value;
-			updateAutoTexts();
-		},
-		get: function() {
-			return element.value;
-		},
-	});
+	element.style.borderStyle = "solid";
+	element.style.borderColor = "black";
+	element.style.borderWidth = "1px";
+	element.style.borderRadius = "5px";
+	element.style.padding = "5px";
 };

1b6b3f3735ab182f2e63856cf4919a20ce097707
Ângelo Otávio Nuffer Nunes <angelonuffer@gmail.com>
2016-02-26 20:44:59 +0000

    Improve text field.

--- scripts/text_field.js
+++ scripts/text_field.js
@@ -1,11 +1,13 @@
 function TextField(name, kwargs) {
-	var element = document.createElement("input");
+	var element = document.createElement("textarea");
 	Field.call(this, name, element, kwargs);
-	element.type = "text";
 	element.style.width = "100%";
 	element.style.borderStyle = "solid";
 	element.style.borderColor = "black";
 	element.style.borderWidth = "1px";
 	element.style.borderRadius = "5px";
-	element.style.padding = "5px";
+	element.style.padding = "7px";
+	element.style.resize = "none";
+	element.rows = 1;
+	autosize(element);
 };
